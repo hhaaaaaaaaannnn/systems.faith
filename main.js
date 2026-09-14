453217b8a53def76527d944b0a8c0dbf70fb167d
Han <handlarzsmiesznygosc@gmail.com>
2026-09-14 17:40:22 +0000

    make

---
+++ main.js
@@ -0,0 +1,15 @@
+function copydc() {
+    const copyText = "HanHayashi"
+
+    navigator.clipboard.writeText(copyText);
+
+    alert("Copied discord handle");
+}
+
+function copyemail() {
+    const copyText = "contact@angxl.eu"
+
+    navigator.clipboard.writeText(copyText);
+
+    alert("Copied email address");
+}
